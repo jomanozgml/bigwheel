852332d222c68068864b214bde0727467c7a764f
manoz <jomanozgml@gmail.com>
2023-04-07 12:24:09 +1000

    Builded and deployed to firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 final counterProvider = StateProvider.autoDispose((ref) => 0);
 
-void main() {
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
